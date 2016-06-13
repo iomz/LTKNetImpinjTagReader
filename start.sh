73db32e986b3377cb99eba7bb6baf3538983e048
Iori Mizutani <iori.mizutani@gmail.com>
2016-06-13 21:33:02 +0900

    Fix Indentation for specs

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-cd bin/Debug && /usr/bin/mono LTKNetImpinjTagReader.exe 203.178.141.51 10000
+#cd bin/Debug && mono LTKNetImpinjTagReader.exe 203.178.141.51 10000
+#cd bin/Debug && mono LTKNetImpinjTagReader.exe 169.254.1.1 5000
+cd bin/Debug && mono LTKNetImpinjTagReader.exe 203.178.128.217 1000
